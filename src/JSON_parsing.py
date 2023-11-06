da21da1684e9e2cb104416cf0b9a7eaf2895323b
AmyGrimwood <amy@grimwood.me.uk>
2023-11-06 16:23:44 +0000

    Removed all caps in variable names

--- src/JSON_parsing.py
+++ src/JSON_parsing.py
@@ -22,40 +22,40 @@ class Parser():
     def extract_panel_id(self,input_json):
 
         try:
-            Panel_id = input_json['id']
+            panel_id = input_json['id']
         except KeyError:
             print('KeyError:PanelApp output JSON doesn\' contain \'id\' key')
-            Panel_id = np.nan
+            panel_id = np.nan
 
-        return Panel_id
+        return panel_id
     
     #Extract panel version from json, including error handelling for jsons missing the 'version' key
     def extract_version(self,input_json):
         try:
-            Panel_version = input_json['version']
+            panel_version = input_json['version']
         except KeyError:
             print('KeyError:PanelApp output JSON doesn\' contain \'version\' key')
-            Panel_version = np.nan
-        return Panel_version
+            panel_version = np.nan
+        return panel_version
     
      #Extract Disease from json, including error handelling for jsons missing the 'relevant_disorders' key
-    def extract_Disease(self,input_json):
+    def extract_disease(self,input_json):
         try:
-            Disease = input_json['relevant_disorders']
+            disease = input_json['relevant_disorders']
         except KeyError:
             print('KeyError:PanelApp output JSON doesn\' contain \'relevant_disorders\' key')
-            Disease = np.nan
-        return Disease
+            disease = np.nan
+        return disease
 
     #Extract Date Last Updated from json, including error handelling for jsons missing the 'version_created' key
     def extract_last_updated(self,input_json):
         try:
-            Last_updated = input_json['version_created']
-            Last_updated = Last_updated[:Last_updated.index("T")]
+            last_updated = input_json['version_created']
+            last_updated = last_updated[:last_updated.index("T")]
         except KeyError:
             print('KeyError:PanelApp output JSON doesn\' contain \'version_created\' key')
-            Last_updated = np.nan
-        return Last_updated
+            last_updated = np.nan
+        return last_updated
     
     #Extract genes on panel from json, including error handelling for jsons mimissing the 'genes', 'gene_data' or 'hgnc_symbol' key>
     def extract_genes(self,input_json):
