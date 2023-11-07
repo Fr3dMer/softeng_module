46eb751108e602cbc2eab413828fb39f0a1c9d4a
fr3dmer <mercerfrederick@gmail.com>
2023-11-07 16:43:13 +0000

    Added readme + finished requests

---
+++ README.md
@@ -0,0 +1,20 @@
+# PannelAppDB
+
+## Install 
+```bash
+git clone https://github.com/Fr3dMer/softeng_module.git
+conda env create -n PannelAppDB --file environment.yml
+conda activate PannelAppDB
+```
+
+## Running script 
+```bash
+
+
+```
+
+## Tests
+```bahs
+
+
+```
--- src/api.py
+++ src/api.py
@@ -7,46 +7,43 @@ Author        : Freddie Mercer
 import requests
 
 
-# Needs logic for checking version of pannel is most up to date 
-
-
-# Check which pannel is up to date 
-# Collect all pannel versions
-# Collect pannel data based on version 
-# Collect detailed pannel data for provided pannel 
+# Object to make call to gel pannel app rest api
 class api_obj():
 
-
+    # Constructor 
     def __init__(self,args):
         self.args = args
 
         url = "https://panelapp.genomicsengland.co.uk/api/v1/panels/3/?version=4.0"
 
+    # Call endpoint to get most up to date version of signed off pannel 
+    def get_all_pannels(self,pannel_id):
 
-    def get_all_pannels(self):
+        self.value_checker(pannel_id=pannel_id)
 
         url = "https://panelapp.genomicsengland.co.uk/api/v1/panels/signedoff/"
+        payload = {'panel_id':pannel_id} 
 
-        return requests.get(url)
+        return requests.get(url,params=payload)
 
+    # Get detailed info on individual pannel
     def get_single_detailed_pannel(self,pannel_id,version):
 
-        if(type(version) == int):
-            print("incorrect value type for version provided, please use a string or float")
-            raise SystemError
+        self.value_checker(version=version,pannel_id=pannel_id)
 
         url = "https://panelapp.genomicsengland.co.uk/api/v1/panels/3/"       
         payload = {'version':version} 
 
         return requests.get(url,params=payload)
 
-    def value_checker(self,pannel_id,version):
+    # Support func to ensure variables in correct format 
+    def value_checker(self,pannel_id,version=""):
 
         if(type(version) == int):
             print("incorrect value type for version provided, please use a string or float")
             raise SystemError
         
-        if(type(pannel_id) == int):
+        if(type(pannel_id) != int):
             print("incorrect value type for version provided, please use a string or float")
             raise SystemError
 
@@ -56,9 +53,11 @@ class api_obj():
 if (__name__ == "__main__"):
     api = api_obj("arg")
 
-    result = api.get_single_detailed_pannel(3,4)
+    result = api.get_single_detailed_pannel(3,4.0)
+    result2 = api.get_all_pannels(3)
 
-    print(result.json())
+    print(result.json(),"\n\n")
+    print(result2.json())
 
 
 
