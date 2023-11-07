276e15de7a2ec65718e5e6639b2fd2f1a165dd99
fr3dmer <mercerfrederick@gmail.com>
2023-11-07 14:41:48 +0000

    First commit

---
+++ src/api.py
@@ -0,0 +1,18 @@
+"""****************************************************************************
+File          : api.py
+About         : Object for interacting with pannelapp api 
+Author        : Freddie Mercer
+****************************************************************************"""
+
+
+
+# Needs logic for checking version of pannel is most up to date 
+
+
+# Check which pannel is up to date 
+# Collect all pannel versions
+# Collect pannel data based on version 
+# Collect detailed pannel data for provided pannel 
+
+
+
