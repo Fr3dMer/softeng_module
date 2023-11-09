61032d52e11bd70e0c6b6a8244776008a6187459
fr3dmer <mercerfrederick@gmail.com>
2023-11-09 14:11:55 +0000

    Starting on general structure

--- src/api.py
+++ src/api.py
@@ -3,7 +3,7 @@ File          : api.py
 About         : Object for interacting with pannelapp api 
 Author        : Freddie Mercer
 ****************************************************************************"""
-
+from urllib import request
 import requests
 
 # Object to make call to gel pannel app rest api
@@ -14,7 +14,7 @@ class api_obj():
         self.args = args
 
     # Call endpoint to get most up to date version of signed off pannel 
-    def get_all_pannels(self,pannel_id):
+    def get_gms_pannel(self,pannel_id):
 
         self.value_checker(pannel_id=pannel_id)
 
@@ -43,29 +43,34 @@ class api_obj():
             raise SystemError("incorrect value type for version provided, please use a int")
     
     def version_check(self,query_version,true_version):
-
+        
         # Make sure versions are correct type 
         if not (type(query_version) == float and type(true_version) == float):
             raise SyntaxError("Verions are not floats, cannot compare versions")
 
         # If versions are equal 
-        if(query_version == true_version):
+        if(query_version == true_version):            
+            
             return True
 
         # 2.3 > 5 == True
-        # 
         elif(query_version > true_version):
-            pass 
+            pass    
         
         # 5 < 2.3 == True
         elif(query_version < true_version):
             pass
+    
+    # Check internet connection 
+    def check_internet(self):
+        try:
+            request.urlopen('http://216.58.192.142', timeout=1)
+            return True
+        except request.URLError as err: 
+            return False
 
 
 
-
-
-        
 
 
 
