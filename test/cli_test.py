818fd0c5d43a94f64fc1aedcdb55b3e40f39d093
fr3dmer <mercerfrederick@gmail.com>
2023-12-01 15:30:31 +0000

    Finished CLI test

--- test/cli_test.py
+++ test/cli_test.py
@@ -12,9 +12,13 @@ import src.cli as cli_obj
 # Setup test enviro 
 #-----------------------------------------------------------------------------
 
-normal_pan = ['-p','123']
-normal_rcode = ['-r','place']
+normal_pan = ['-p',
+			  '123']
+normal_rcode = ['-r',
+				'place']
 normal = normal_pan + normal_rcode + ['-H']
+no_panelid = ['-p'] + normal_rcode
+no_rcode = normal_pan + ['-r']
 
 
 class TestApi():
@@ -29,7 +33,22 @@ class TestApi():
 	def test_cli1_init(self):
 		
 		cli = cli_obj.cli_obj(normal)
+		assert cli.args.panel_id == int(normal_pan[1])
+		assert cli.args.rcode == normal_rcode[1]
 		assert cli.args.human == True
 	
+	# Neither value present returns  error 
 	def test_cli2_init(self):
-		pass
+		
+		with pytest.raises(SystemExit):
+			cli_obj.cli_obj(no_panelid)
+		
+		with pytest.raises(SystemExit):
+			cli_obj.cli_obj(no_rcode)
+
+	# Finally test Human bool flag 
+	def test_cli2_init(self):
+
+		cli = cli_obj.cli_obj(normal[0:-1])
+		assert cli.args.human == False
+
