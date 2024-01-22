3ba2d5564e393faa8352a8a9a981e08be8f92e64
fr3dmer <mercerfrederick@gmail.com>
2024-01-22 11:26:33 +0000

    Refactored and moved GMS checking code into seperate function

--- PannelAppDB.py
+++ PannelAppDB.py
@@ -4,39 +4,17 @@ About         : Hold main logical flow for app
 Author        : Freddie Mercer
 ****************************************************************************"""
 
+import sys
 import src.api as api_module
 import src.cli as cli_module
 import src.JSON_parsing as parser_obj
-import sys
 
 
-def main():
+# Extracts version from raw_data, calls api to ensure most recent GMS panel, 
+# if not gets most recent GMS panel
+def get_gms_versions(raw_data,parser,api):
 
-    # Initiaslise CLI obj
-    cli = cli_module.cli_obj(sys.argv[1:])
-    
-    # Instantiate api obj and parser
-    api = api_module.api_obj("args")
-    parser = parser_obj.Parser("args")
-
-    # Check internet connection
-    internet_status = api.check_internet()
-
-    # Call pannel from db, returning version
-    if not(internet_status==True):
-        raise SystemExit("Could not connect to internet")
-    
-    # Check what has been passed in, preferentialy start with panel_id for now
-    if(type(cli.args.panel_id) == int):
-        raw_data = api.get_single_detailed_pannel_id(cli.args.panel_id)
-
-    elif(type(cli.args.rcode) == str):
-        raw_data = api.get_single_detailed_pannel_rcode(cli.args.rcode)
-    
-    else:
-        raise SystemExit("Panel id or Rcode must be entered")
-
-    # Check what has been passed in, preferentialy start with panel_id for now
+    # Make a call to API and get GMS version
     query_version = float(parser.extract_version(raw_data))
     query_id = int(parser.extract_panel_id(raw_data))
     gms_panel = api.get_gms_pannel(query_id)
@@ -47,14 +25,49 @@ def main():
         # If GMS version has changed, get new version and push to db, return 
         # new version to variable
         # Call GMS pannel version
-        raw_data = api.get_single_detailed_pannel_id(query_id,gms_pannel_version)
+        return api.get_single_detailed_pannel_id(query_id,gms_pannel_version)
+    else:
+        return raw_data
 
 
+def main():
+
+    # Initiaslise CLI obj
+    cli = cli_module.cli_obj(sys.argv[1:])
+    
+    # Instantiate api obj, parser and DB
+    api = api_module.api_obj("args")
+    parser = parser_obj.Parser("args")
+
+    # Check internet connection
+    internet_status = api.check_internet()
+
+    # If no internet, get panel from db
+    if not(internet_status==True):
+        raise SystemExit("Could not connect to internet")
+    
+    # Otherwise using panel id, get most recent panel
+    elif(type(cli.args.panel_id) == int):
+        raw_data = api.get_single_detailed_pannel_id(cli.args.panel_id)
+        raw_data = get_gms_versions(raw_data,parser,api)
+
+    # If no panel id, get most recent panel using R-code
+    elif(type(cli.args.rcode) == str):
+        raw_data = api.get_single_detailed_pannel_rcode(cli.args.rcode)
+        raw_data = get_gms_versions(raw_data,parser,api)
+    
+    else:
+        raise SystemExit("Panel id or Rcode must be entered")
+
+    # Re parse all data again
     used_version = raw_data.get("version",None)
     disease = parser.extract_disease(raw_data)
     updated = parser.extract_last_updated(raw_data)
     genes = parser.extract_genes(raw_data)
 
+    # Send parsed data to db
+
+    # Print all parsed data
     print("Panel id :          ",query_id)
     print("Version :           ",used_version)
     print("Disease:            ",disease[0])
