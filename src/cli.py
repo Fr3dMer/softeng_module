0cd87ee95512ec56bdf2d8a38aa29674b3892e48
fr3dmer <mercerfrederick@gmail.com>
2024-01-23 18:41:21 +0000

    Bringning cli.py up to pep8

--- src/cli.py
+++ src/cli.py
@@ -8,8 +8,23 @@ import argparse
 
 
 class cli_obj():
+
+    """ Obj for parsing and holding params passed in at start.
+    Organisation:
+    __init__(): Construct parser with appropriate arguments and 
+                then parse.
+    check_patient(): Error catching logic to prevent incompatable 
+                     combinations of arguments.
+    """
     
     def __init__(self,raw_args):
+
+        """Construct parser with appropriate arguments and 
+        then parse.
+        
+        Keyword arguments:
+        raw_args (list): output of sys.argv[1:]
+        """
         
         parser = argparse.ArgumentParser(description = "An app for pulling NGS pannel data")
         
@@ -21,10 +36,14 @@ class cli_obj():
                             "--rcode",
                             type=str,
                             help="Search for pannel by rcode")
-        parser.add_argument("-H",
-                            "--human",
-                            action="store_true",
-                            help="View data in human readable format")
+        parser.add_argument("-pid",
+                            "--patientID",
+                            type=str,
+                            help="PatientID to insert into db")
+        parser.add_argument("-sid",
+                            "--sampleID",
+                            type=str,
+                            help="SampleID to insert into db")
         
         self.args = parser.parse_args(raw_args)
 
@@ -35,11 +54,22 @@ class cli_obj():
             self.return_panel_info = False
 
         # Check if any patient details enteres
-        if (type(self.args.patientID) == str or type(self.args.sampletID) == str):
+        if (type(self.args.patientID) == str or type(self.args.sampleID) == str):
             self.patient_info == True
         else:
             self.patient_info == False
 
+    def check_patient(self):
+
+        """Error catching logic to prevent incompatable 
+        combinations of arguments.
+        
+        Keyword arguments:
+        None needed
+
+        Returns:
+        Bool: True if issues, False if no incompatabilities
+        """
 
 
 # - Need to add error catching for if patient ID suplied withought sample ID and vice versa
