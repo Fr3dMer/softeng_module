8bc76b04a10888e24dd39882e90012a7fd345862
AmyGrimwood <73721119+AmyGrimwood@users.noreply.github.com>
2024-02-15 16:42:30 +0000

    added docstrings to JSON_parsing.py

--- src/JSON_parsing.py
+++ src/JSON_parsing.py
@@ -1,15 +1,28 @@
+"""****************************************************************************
+File          : JSON_parsing.py
+About         : Parse information from PanelApp API to human readable text 
+Author        : Freddie Mercer
+****************************************************************************"""
+
+"""This is the JSON_parsing module.
+It parses data from the raw JSON returned from the pannel app API to extract 
+pannel ID, pannel version, relevant disorders, date updated, genes included on
+the pannel and collates the data into a JSON to be entered into the database.
+"""
+
+#Import required modules
 import json
 import pandas as pd
 import numpy as np
 
 class Parser():
-
+    """Class extracts required data from Pannel app api outputs and recombines into JSON"""
 
     def __init__(self,args):
         self.args = args
 
     def extract_panel_id(self,input_json):
-
+        """Extract panel id from json, including error handelling for jsons missing the 'id' key"""
         try:
             panel_id = input_json['id']
         except KeyError:
@@ -19,6 +32,9 @@ class Parser():
         return panel_id
 
     def extract_version(self,input_json):
+        """Extract panel version from json, including 
+        error handelling for jsons missing the 'version' key
+        """
         try:
             panel_version = input_json['version']
         except KeyError:
@@ -27,6 +43,9 @@ class Parser():
         return panel_version
 
     def extract_disease(self,input_json):
+        """Extract Disease from json, including error handelling for 
+        jsons missing the 'relevant_disorders' key
+        """
         try:
             disease = input_json['relevant_disorders']
         except KeyError:
@@ -35,6 +54,9 @@ class Parser():
         return disease
 
     def extract_last_updated(self,input_json):
+        """Extract Date Last Updated from json, including 
+        error handelling for jsons missing the 'version_created' key
+        """
         try:
             last_updated = input_json['version_created']
             last_updated = last_updated[:last_updated.index("T")]
@@ -44,11 +66,17 @@ class Parser():
         return last_updated
     
     def extract_genes(self,input_json):
+        """Extract genes on panel from json, including error 
+        handelling for jsons mimissing the 'genes', 'gene_data' 
+        or 'hgnc_symbol' key
+        """
         try:
             gene_info = input_json['genes']
             gene_list = []
             for x in gene_info:
-                #Given a key, the get() method returns paired value from dict (documentation found at https://docs.python.org/2/library/stdtypes.html in section 5.8)
+                #Given a key, the get() method returns paired value from
+                #dict (documentation found at https://docs.python.org/2/library/stdtypes.html 
+                #in section 5.8)
                 hgnc_symbol = x.get('gene_data',{}).get('hgnc_symbol')
                 hgnc_id = x.get('gene_data',{}).get('hgnc_id')
                 GRch38_coord = (x.get('gene_data',{}).get('ensembl_genes',{})
@@ -63,6 +91,9 @@ class Parser():
         return gene_list
 
     def generate_bed(self,input_json,ref_seq='grch38'):
+        """Generate BED file text, including error handelling 
+        for jsons mimissing the 'genes' or 'location' key
+        """
         try:
             gene_info = input_json['genes']
             location_list = []
